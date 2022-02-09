4a7c08b22aaad6160532be34f69eaa01a7d2016f
rudoyo <krimotop2016@gmail.com>
2022-02-09 15:02:29 +0100

    --all

--- JS.js
+++ JS.js
@@ -1,5 +1,5 @@
 let opponent = {};
-const players = {};
+const players = { xPlacment: [], oPlacment: [] };
 let opponentPicking = function () {
   let buttons = [...document.querySelectorAll(".oponentbtn")];
   const opponentPickingDiv = document.querySelector(".opponentPicking");
@@ -28,23 +28,23 @@ let opponentPicking = function () {
         playersNames.style.display = "grid";
         showinut.classList.add("active");
         const play = document.querySelector(".play");
-        const input2 = document.querySelector("#sPlayer");
-        const input1 = document.querySelector("#fPlayer");
+        const input2 = document.querySelector("#oPlayer");
+        const input1 = document.querySelector("#xPlayer");
 
         play.addEventListener("click", function () {
           if (!(input1.validity.valid || input2.validity.valid)) {
             const emailError = document.querySelector(".error");
             emailError.textContent = "enter both names to proceed";
           } else if (input1.validity.valid && input2.validity.valid) {
-            players.fPlayer = input1.value;
-            players.sPlayer = input2.value;
+            players.xPlayer = input1.value;
+            players.oPlayer = input2.value;
             console.log(players);
             playersNames.style.display = "none";
             //display gameBoard
             const gameBoard = document.querySelectorAll(".gameboard > div");
             gameBoard.forEach((item) => (item.style.display = "grid"));
-            document.querySelector("#pA").textContent = players.fPlayer;
-            document.querySelector("#pB").textContent = players.sPlayer;
+            document.querySelector("#pA").textContent = players.xPlayer + "(x)";
+            document.querySelector("#pB").textContent = players.oPlayer + "(o)";
             playrules();
           }
         });
@@ -52,6 +52,8 @@ let opponentPicking = function () {
     })
   );
 };
+
+let playerSign = "❌";
 opponentPicking();
 const gameBoardcells = document.querySelectorAll(".gameboard >div");
 function playrules() {
@@ -59,13 +61,50 @@ function playrules() {
   gameBoardcells.forEach((item) =>
     item.addEventListener("click", function (e) {
       console.log(e.target);
-      if ((e.currentTarget.innerText = "⭕")) {
-        e.currentTarget.innerText = "❌";
-      } else if ((e.currentTarget.innerText = "❌")) {
-      } else if (e.currentTarget.innerText == "") {
-      } else {
-        e.currentTarget.innerText = "⭕";
+
+      if (e.currentTarget.textContent == "⭕") {
+      } else if (e.currentTarget.textContent == "❌") {
+      } else if (e.currentTarget.textContent == "") {
+        if (playerSign == "❌") {
+          playerSign = "⭕";
+          players.oPlacment.push(e.currentTarget.id);
+        } else {
+          playerSign = "❌";
+
+          players.xPlacment.push(e.currentTarget.id);
+        }
+
+        console.log(players.xPlacment);
+        console.log(players.oPlacment);
+        e.currentTarget.textContent = playerSign;
+        checkWinner(e);
       }
     })
   );
 }
+const checkWinner = function () {
+  const winConditions = [
+    ["0", "1", "2"],
+    ["3", "4", "5"],
+    ["6", "7", "8"],
+    ["0", "3", "6"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["0", "4", "8"],
+    ["2", "4", "6"],
+  ];
+
+  if (
+    winConditions.filter((items) =>
+      items.every((item) => players.xPlacment.includes(item))
+    ).length != 0
+  ) {
+    console.log("the winner is" + players.xPlayer);
+  } else if (
+    winConditions.filter((items) =>
+      items.every((item) => players.oPlacment.includes(item))
+    ).length != 0
+  ) {
+    console.log("the winner is " + players.oPlayer);
+  }
+};
